041434d1cc6f1a8bc273e0551fe33b16db1d9980
alxayeed <alxayeed@gmail.com>
2019-11-11 18:12:56 +0600

    new fields added to the tables

--- example/models.py
+++ example/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 class Company(models.Model):
 	name = models.CharField(max_length =20)
+	location = models.CharField(max_length=20)
+	date_created = models.DateField()
 
 	def __str__(self):
 		return self.name
 
 class Language(models.Model):
 	name = models.CharField(max_length=20)
+	creator = models.CharField(max_length=20)
+	paradigm = models.CharField(max_length=20)
+	date_created = models.DateField(blank=True)
 	
 
 	def __str__(self):
@@ -15,6 +20,7 @@ class Language(models.Model):
 
 class Programmer(models.Model):
 	name = models.CharField(max_length=20)
+	age = models.IntegerField()
 	company = models.ForeignKey(Company,on_delete=models.CASCADE)
 	language = models.ManyToManyField(Language)
 
