0374824c0e4aaa8033a6dc6ed2487bde9f94d0db
alxayeed <alxayeed@gmail.com>
2019-11-11 13:56:09 +0600

    new table Programmer added with the foreignkey relationship with Company

--- example/models.py
+++ example/models.py
@@ -3,3 +3,13 @@ from django.db import models
 class Company(models.Model):
 	name = models.CharField(max_length =20)
 
+	def __str__(self):
+		return self.name
+
+class Programmer(models.Model):
+	name = models.CharField(max_length=20)
+	company = models.ForeignKey(Company,on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
